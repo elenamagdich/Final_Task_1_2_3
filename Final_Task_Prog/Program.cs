e4c27f919a7b2b037a42bab01316d6291d943704
Elena <elena_magdich@mail.ru>
2023-04-04 20:09:39 +0300

    Final_Task_Prog_

---
+++ Final_Task_Prog/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк
+// формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+Console.Write("Введите количество элементов Вы в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayMassive = new string[size];
+int M = 3;
+int N = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string sumbols = Convert.ToString(Console.ReadLine());
+    if (sumbols.Length <= M)
+    {
+        arrayMassive[N] = sumbols;
+        N++;
+    }
+}
+Console.WriteLine();
+PrintArray(arrayMassive);
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
